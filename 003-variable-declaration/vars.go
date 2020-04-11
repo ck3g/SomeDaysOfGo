bfbc114fb18989607e6b73cc0821d8b7fe17653c
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-11 12:55:26 +0200

    Declare variables using var keyword

--- 003-variable-declaration/vars.go
+++ 003-variable-declaration/vars.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
+var x0 = 40
+
+// y0 := 30 // syntax error: non-declaration statement outside function body
+
+var z0 int // Declares a variable of type `int` and set it to Zero value
+
 func main() {
 	var x = 42 // variable declaration
-	fmt.Println(x)
+	fmt.Println("x =", x)
 	x = 99 // reassign variable
-	fmt.Println(x)
+	fmt.Println("x =", x)
 
 	y := 43 // short variable declaration
-	fmt.Println(y)
+	fmt.Println("y =", y)
 
 	z := "Define " + "expression"
-	fmt.Println(z)
+	fmt.Println("z =", z)
+
+	fmt.Println("what is z0?", z0)
 }
