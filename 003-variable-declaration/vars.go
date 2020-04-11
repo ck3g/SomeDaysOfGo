7ef3a797a6e9e98d46db4e7f302e3dd4b963ba7d
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-11 12:09:06 +0200

    Variable declaration

---
+++ 003-variable-declaration/vars.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x = 42 // variable declaration
+	fmt.Println(x)
+	x = 99 // reassign variable
+	fmt.Println(x)
+
+	y := 43 // short variable declaration
+	fmt.Println(y)
+
+	z := "Define " + "expression"
+	fmt.Println(z)
+}
