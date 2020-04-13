c0e5d6c82f7403fbf17155c2b06e61e94a9e15d6
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-13 14:51:22 +0200

    Use buffered channels

--- 040-channels/main.go
+++ 040-channels/main.go
@@ -14,4 +14,13 @@ func main() {
 	}()
 
 	fmt.Println(<-c)
+
+	// Version 3
+	bc := make(chan int, 1) // Buffered channel, allows a 1 value to sit in there
+
+	bc <- 504 // This time it works, because the channel is buffered
+	// bc <- 505 // Can't put more values into this channel
+
+	fmt.Println(<-bc)
+
 }
