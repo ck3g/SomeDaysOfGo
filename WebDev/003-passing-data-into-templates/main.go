e905bd2bccade2aa3c949655f384f3780906934c
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-15 10:41:08 +0200

    Using a variable in a template

--- WebDev/003-passing-data-into-templates/main.go
+++ WebDev/003-passing-data-into-templates/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"text/template"
@@ -16,6 +17,13 @@ func init() {
 
 func main() {
 	err := tpl.ExecuteTemplate(os.Stdout, "simple_data.gohtml", 42)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("\n-------")
+
+	err = tpl.ExecuteTemplate(os.Stdout, "using_variable.gohtml", `Release self-focus; embrace other-focus.`)
 	if err != nil {
 		log.Fatalln(err)
 	}
