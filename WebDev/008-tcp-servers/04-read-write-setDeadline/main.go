c014b6b774ea7e7cff0ea7d7659d3f777814ed17
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-15 22:07:29 +0200

    TCP server with setDeadline

---
+++ WebDev/008-tcp-servers/04-read-write-setDeadline/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(20 * time.Second))
+	if err != nil {
+		log.Fatalln("CONN TIMEOUT")
+	}
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+	defer conn.Close()
+
+	// we never get here
+	// we have an open stream connection
+	// how does the above reader know when it's done?
+	fmt.Println("Code got here.")
+}
+
+// use `$ telnet localhost 8080` and start sending messages to test
