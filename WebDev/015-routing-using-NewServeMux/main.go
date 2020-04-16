1634046e6205343f9cb191f5d10d7b9002573500
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-16 15:22:19 +0200

    Routing using http.NewServeMux

---
+++ WebDev/015-routing-using-NewServeMux/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+type anything int
+
+func (a anything) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "Anything")
+}
+
+type anythingElse int
+
+func (a anythingElse) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "Anything else")
+}
+
+func main() {
+	var a anything
+	var e anythingElse
+
+	mux := http.NewServeMux()
+	mux.Handle("/", a)
+	mux.Handle("/about", e)
+
+	http.ListenAndServe(":8080", mux)
+}
