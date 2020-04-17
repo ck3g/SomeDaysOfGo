5b830e43e9567f37f8bf6af6a338ff82797d5cfd
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-17 14:36:38 +0200

    Generate UUID and store in a cookie

---
+++ WebDev/025-uuid/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net/http"
+
+	// go get github.com/satori/go.uuid
+	uuid "github.com/satori/go.uuid"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("session")
+	if err != nil {
+		id, _ := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name:  "session",
+			Value: id.String(),
+			// Secure: true,
+			HttpOnly: true,
+			Path:     "/",
+		}
+		http.SetCookie(w, cookie)
+	}
+
+	io.WriteString(w, cookie.String())
+}
