bd9051185d38419a79baf141c7a01deba2270a09
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-17 15:46:23 +0200

    Encrypt a password with bcrypt

---
+++ WebDev/028-password-bcrypt/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+
+	// go get github.com/satori/go.uuid
+	uuid "github.com/satori/go.uuid"
+)
+
+type user struct {
+	UserName string
+	Password []byte
+	First    string
+	Last     string
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{}      // user ID, user
+var dbSessions = map[string]string{} // session ID, user ID
+
+func init() {
+	tpl = template.Must(template.ParseGlob("WebDev/028-password-bcrypt/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/signup", signup)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	usr := getUser(req)
+	tpl.ExecuteTemplate(w, "index.gohtml", usr)
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	usr := getUser(req)
+	if !alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "bar.gohtml", usr)
+}
+
+func signup(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	// process form submission
+	if req.Method == http.MethodPost {
+		// get form values
+		username := req.FormValue("username")
+		password := req.FormValue("password")
+		firstname := req.FormValue("firstname")
+		lastname := req.FormValue("lastname")
+
+		// user taken?
+		if _, ok := dbUsers[username]; ok {
+			http.Error(w, "Username already taken", http.StatusForbidden)
+			return
+		}
+
+		// create session
+		sID, _ := uuid.NewV4()
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, cookie)
+		dbSessions[cookie.Value] = username
+
+		// Store user in dbUsers
+		bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		usr := user{username, bs, firstname, lastname}
+		dbUsers[username] = usr
+
+		// redirect
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+	}
+
+	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+}
