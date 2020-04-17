f59900b614d8e86fbde51123a89d0f7fd00c1c52
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-17 14:04:25 +0200

    Expire a cookie

--- WebDev/024-cookies/main.go
+++ WebDev/024-cookies/main.go
@@ -6,11 +6,22 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", set)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/set", set)
 	http.HandleFunc("/read", read)
+	http.HandleFunc("/expire", expire)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
+
+func index(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, `
+		<div><a href="/set">Set a cookie</a></div>
+		<div><a href="/read">Read the cookie</a></div>
+		<div><a href="/expire">Expire the cookie</a></div>
+	`)
+}
+
 func set(w http.ResponseWriter, req *http.Request) {
 	http.SetCookie(w, &http.Cookie{
 		Name:  "my-cookie",
@@ -28,4 +39,16 @@ func read(w http.ResponseWriter, req *http.Request) {
 	}
 
 	io.WriteString(w, "YOUR COOKIE"+c.String())
+}
+
+func expire(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("my-cookie")
+	if err != nil {
+		http.Redirect(w, req, "/set", http.StatusSeeOther)
+		return
+	}
+
+	c.MaxAge = -1 // Delete the cookie
+	http.SetCookie(w, c)
+	http.Redirect(w, req, "/", http.StatusSeeOther)
 }
