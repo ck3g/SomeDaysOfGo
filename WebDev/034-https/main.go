c75f9ba75498b4024250c28d014f37506382b107
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-19 09:20:42 +0200

    Add HTTPS example

---
+++ WebDev/034-https/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is an example server.\n"))
+}
+
+// Generate unsigned certificate
+// $ go run $(go env GOROOT)/src/crypto/tls/generate_cert.go --host=localhost
+
+// Visit https://localhost:10443
