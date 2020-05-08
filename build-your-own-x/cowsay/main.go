b116fa7b279e4637c87e1b57837f06ab011a7bcf
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-08 08:02:41 +0200

    Build a balloon

--- build-your-own-x/cowsay/main.go
+++ build-your-own-x/cowsay/main.go
@@ -42,11 +42,9 @@ func main() {
 	lines = tabsToSpaces(lines)
 	maxwidth := calculateMaxWidth(lines)
 	messages := normalizeStringsLength(lines, maxwidth)
+	balloon := buildBalloon(messages, maxwidth)
 
-	for _, message := range messages {
-		fmt.Println(message)
-	}
-
+	fmt.Println(balloon)
 	fmt.Println(cow)
 	fmt.Println()
 }
@@ -88,4 +86,37 @@ func normalizeStringsLength(lines []string, maxwidth int) []string {
 	}
 
 	return ret
+}
+
+// buildBalloon takes a slice of strings of max width maxwidth
+// prepends/appends margins of first and last line, and at start/end of each line
+// and returns a string with the contents of the balloon
+func buildBalloon(lines []string, maxwidth int) string {
+	var borders []string
+	count := len(lines)
+	var ret []string
+
+	borders = []string{"/", "\\", "\\", "/", "|", "<", ">"}
+
+	top := " " + strings.Repeat("_", maxwidth+2)
+	bottom := " " + strings.Repeat("-", maxwidth+2)
+
+	ret = append(ret, top)
+	if count == 1 {
+		s := fmt.Sprintf("%s %s %s", borders[5], lines[0], borders[6])
+		ret = append(ret, s)
+	} else {
+		s := fmt.Sprintf("%s %s %s", borders[0], lines[0], borders[1])
+		ret = append(ret, s)
+		i := 1
+		for ; i < count-1; i++ {
+			s = fmt.Sprintf("%s %s %s", borders[4], lines[i], borders[4])
+			ret = append(ret, s)
+		}
+		s = fmt.Sprintf("%s %s %s", borders[2], lines[i], borders[3])
+		ret = append(ret, s)
+	}
+
+	ret = append(ret, bottom)
+	return strings.Join(ret, "\n")
 }
