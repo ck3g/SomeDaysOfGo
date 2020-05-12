61294276373d8ae82850ba5b17c18cffda87cdd2
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-12 07:05:17 +0200

    Calculate the number of commits per day

--- build-your-own-x/gitlocalstats/stats.go
+++ build-your-own-x/gitlocalstats/stats.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-git/go-git"
+	"github.com/go-git/go-git/plumbing/object"
+)
+
+const daysInLastSixMonths = 183
+const outOfRange = 99999
 
 // stats calcualtes and prints the stats
 func stats(email string) {
@@ -9,10 +18,113 @@ func stats(email string) {
 }
 
 func processRepositories(email string) map[int]int {
-	fmt.Println(repositories)
-	return make(map[int]int)
+	daysInMap := daysInLastSixMonths
+
+	commits := make(map[int]int, daysInMap)
+	for i := daysInMap; i > 0; i-- {
+		commits[i] = 0
+	}
+
+	for _, path := range repositories {
+		commits = fillCommits(email, path, commits)
+	}
+
+	return commits
+}
+
+// fillCommits given a repository found in `path`, gets the commits and
+// puts them in the `commits` map, returning it when completed
+func fillCommits(email string, path string, commits map[int]int) map[int]int {
+	// instantiate a git repo object from path
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		panic(err)
+	}
+
+	// get the HEAD reference
+	ref, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+
+	// get the commits history starting from HEAD
+	iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		panic(err)
+	}
+
+	// iterate the commits
+	offset := calcOffset()
+	err = iterator.ForEach(func(c *object.Commit) error {
+		daysAgo := countDaysSinceDate(c.Author.When) + offset
+
+		if c.Author.Email != email {
+			return nil
+		}
+
+		if daysAgo != outOfRange {
+			commits[daysAgo]++
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return commits
+}
+
+// getBeginningOfDay given a time.Time calculates the start time of that day
+func getBeginningOfDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	startOfDay := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+
+	return startOfDay
+}
+
+// countDaysSinceDate counts how many days passed since the passed `date`
+func countDaysSinceDate(date time.Time) int {
+	days := 0
+	now := getBeginningOfDay(time.Now())
+	for date.Before(now) {
+		date = date.Add(time.Hour * 24)
+		days++
+		if days > daysInLastSixMonths {
+			return outOfRange
+		}
+	}
+
+	return days
+}
+
+// calcOffset determines and returns the amount of days missing to fill
+// the last row of the stats graph
+func calcOffset() int {
+	var offset int
+	weekday := time.Now().Weekday()
+
+	switch weekday {
+	case time.Sunday:
+		offset = 7
+	case time.Monday:
+		offset = 6
+	case time.Tuesday:
+		offset = 5
+	case time.Wednesday:
+		offset = 4
+	case time.Thursday:
+		offset = 3
+	case time.Friday:
+		offset = 2
+	case time.Saturday:
+		offset = 1
+	}
+
+	return offset
 }
 
 func printCommitsStats(commits map[int]int) {
-
+	fmt.Println(commits)
 }
