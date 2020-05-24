83885126df06f0052fa5e7b3573f4c6802ae581d
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-24 10:17:42 +0200

    Implement GreetEveryone on the client

--- gRPC-Master-Class/01-greet/greet_client/client.go
+++ gRPC-Master-Class/01-greet/greet_client/client.go
@@ -24,7 +24,8 @@ func main() {
 
 	// doUnary(c)
 	// doServerStreaming(c)
-	doClientStreaming(c)
+	// doClientStreaming(c)
+	doBiDiStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -119,4 +120,56 @@ func doClientStreaming(c greetpb.GreetServiceClient) {
 	}
 
 	fmt.Printf("LongGreetResponse %v\n", res.Result)
+}
+
+func doBiDiStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a BiDi Streaming RPC...")
+
+	// Create a stream by invoking the server
+	stream, err := c.GreetEveryone(context.Background())
+	if err != nil {
+		log.Fatalf("Error while creating stream: %v", err)
+		return
+	}
+
+	waitc := make(chan struct{})
+
+	names := []string{"John", "Bob", "Alice", "Water", "Karl"}
+
+	// Send a bunch of messages to the server
+	go func() {
+		// function to send a bunch of messages
+		for _, name := range names {
+			req := &greetpb.GreetEveryoneRequest{
+				Greeting: &greetpb.Greeting{
+					FirstName: name,
+				},
+			}
+			fmt.Printf("Sending message: %v\n", req)
+			stream.Send(req)
+			time.Sleep(1000 * time.Millisecond)
+		}
+		stream.CloseSend()
+	}()
+
+	// Receive a bunch of messages from the server
+	go func() {
+		// function to receive a bunch of messages
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error while receiving: %v\n", err)
+				break
+			}
+			fmt.Printf("Received: %v\n", res.GetResult())
+		}
+
+		close(waitc)
+	}()
+
+	// Block until everything is done
+	<-waitc
 }
