4ad720b5923341444548dd9958b89b138a889c20
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-26 07:36:07 +0200

    Generate medium addition questions

--- hands-on/math-quiz-generator/main.go
+++ hands-on/math-quiz-generator/main.go
@@ -3,26 +3,43 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	count := 60
-	questions := easyAdditionQuestions(count)
-	for _, q := range questions {
+	for i := 0; i < count; i++ {
+		q := mediumAdditionQuestion()
 		fmt.Println(q)
 	}
-}
-
-func easyAdditionQuestions(count int) []string {
-	var questions []string
-	for i := 0; i < count; i++ {
-		questions = append(questions, easyAdditionQuestion())
-	}
-	return questions
 }
 
 func easyAdditionQuestion() string {
 	first := rand.Intn(9)
 	second := rand.Intn(9)
 	return fmt.Sprintf("%d + %d = __", first, second)
+}
+
+func mediumAdditionQuestion() string {
+	first := randomInRange(7, 20)
+	min := 2
+
+	if first < 10 {
+		min = 8
+	}
+
+	second := randomInRange(min, 10)
+
+	if rand.Intn(2) == 1 {
+		first, second = second, first
+	}
+
+	return fmt.Sprintf("%d + %d = __", first, second)
+}
+
+// randomInRange generates a random number in min...max range
+func randomInRange(min int, max int) int {
+	return rand.Intn(max-min+1) + min
 }
