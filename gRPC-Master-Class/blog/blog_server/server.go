207a7b502a2cd1e0c1602363ec97f0b21e5fee54
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-29 08:51:18 +0200

    Establish connection to MongoDB

--- gRPC-Master-Class/blog/blog_server/server.go
+++ gRPC-Master-Class/blog/blog_server/server.go
@@ -1,25 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/ck3g/SomeDaysOfGo/gRPC-Master-Class/blog/blogpb"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
 type server struct{}
 
+var collection *mongo.Collection
+
 func main() {
 	// if we crash the go code, we get the file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	// connect to MongoDB
+	fmt.Println("Connecting to MongoDB...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatalf("Cannot connect to MongoDB: %v\n", err)
+		return
+	}
+
+	collection = client.Database("").Collection("blog")
+
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("Failied to listen: %v", err)
+		return
 	}
 
 	s := grpc.NewServer()
@@ -27,7 +46,7 @@ func main() {
 	blogpb.RegisterBlogServiceServer(s, &server{})
 
 	go func() {
-		fmt.Println("Starting server...")
+		fmt.Println("Starting server... ")
 
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("Failed to serve: %v", err)
