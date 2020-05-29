6b6417fc9baea40852aa4c83f199a7a570489f24
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-29 08:14:58 +0200

    Add blog server side code

--- gRPC-Master-Class/blog/blog_server/server.go
+++ gRPC-Master-Class/blog/blog_server/server.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
 
+	"github.com/ck3g/SomeDaysOfGo/gRPC-Master-Class/blog/blogpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	fmt.Println("The service is up and running...")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failied to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
