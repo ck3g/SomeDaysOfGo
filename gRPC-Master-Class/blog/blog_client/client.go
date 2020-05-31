4785bba739d2f2c46969de47089ca6a432114830
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-31 11:20:30 +0200

    Implement CreateBlog RPC on the client side

--- gRPC-Master-Class/blog/blog_client/client.go
+++ gRPC-Master-Class/blog/blog_client/client.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
 
+	"github.com/ck3g/SomeDaysOfGo/gRPC-Master-Class/blog/blogpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Running Blog RPC client...")
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := blogpb.NewBlogServiceClient(conn)
+
+	createBlog(c)
+}
+
+func createBlog(c blogpb.BlogServiceClient) {
+	req := &blogpb.CreateBlogRequest{
+		Blog: &blogpb.Blog{
+			AuthorId: "Vitali",
+			Title:    "My First Blog",
+			Content:  "Content of the first blog",
+		},
+	}
+	res, err := c.CreateBlog(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling CreateBlog RPC: %v", err)
+	}
+
+	log.Printf("Blog has been created: %v\n", res)
 }
