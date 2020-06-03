922aa717cd00a294219a986ef85f331da727fc8f
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-03 11:31:28 +0200

    Mock DialService and UserService

---
+++ build-your-own-x/wtf-dial/mock/mock.go
@@ -0,0 +1,45 @@
+package mock
+
+import "github.com/ck3g/SomeDaysOfGo/build-your-own-x/wtf-dial"
+
+// DialService represents a service for managing Dials.
+type DialService struct {
+	DialFn      func(id wtf.DialID) (*wtf.Dial, error)
+	DialInvoked bool
+
+	CreateDialFn      func(dial *wtf.Dial) error
+	CreateDealInvoked bool
+
+	SetLevelFn      func(id wtf.DialID, level float64) error
+	SetLevelInvoked bool
+}
+
+// Dial retrieves dial by ID.
+func (s *DialService) Dial(id wtf.DialID) (*wtf.Dial, error) {
+	s.DialInvoked = true
+	return s.DialFn(id)
+}
+
+// CreateDial creates a new dial.
+func (s *DialService) CreateDial(dial *wtf.Dial) error {
+	s.CreateDealInvoked = true
+	return s.CreateDialFn(dial)
+}
+
+// SetLevel sets the current WTF level for a user.
+func (s *DialService) SetLevel(id wtf.DialID, level float64) error {
+	s.SetLevelInvoked = true
+	return s.SetLevelFn(id, level)
+}
+
+// UserService represents a service fo managing user authentication.
+type UserService struct {
+	AuthenticateFn      func(token string) (*wtf.User, error)
+	AuthenticateInvoked bool
+}
+
+// Authenticate authenticates a user.
+func (s *UserService) Authenticate(token string) (*wtf.User, error) {
+	s.AuthenticateInvoked = true
+	return s.AuthenticateFn(token)
+}
