2677e599d7cfde57d0b1ac756a502cdc4609a8cf
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-04 12:12:03 +0200

    Add bolt internal package

---
+++ build-your-own-x/wtf-dial/bolt/internal/internal.go
@@ -0,0 +1,38 @@
+package internal
+
+import (
+	"time"
+
+	"github.com/ck3g/SomeDaysOfGo/build-your-own-x/wtf-dial"
+	"github.com/golang/protobuf/proto"
+)
+
+//go:generate protoc --experimental_allow_proto3_optional --gogo_out=. internal.proto
+// protoc ./bolt/internal/internal.proto --experimental_allow_proto3_optional --gogo_out=.
+
+// MarshalDial encodes a dial to binary format.
+func MarshalDial(d *wtf.Dial) ([]byte, error) {
+	return proto.Marshal(&Dial{
+		ID:      proto.Int64(int64(d.ID)),
+		UserID:  proto.Int64(int64(d.UserID)),
+		Name:    proto.String(d.Name),
+		Level:   proto.Float64(d.Level),
+		ModTime: proto.Int64(d.ModTime.UnixNano()),
+	})
+}
+
+// UnmarshalDial decodes a dial from a binary data.
+func UnmarshalDial(data []byte, d *wtf.Dial) error {
+	var pb Dial
+	if err := proto.Unmarshal(data, *pb); err != nil {
+		return err
+	}
+
+	d.ID = wtf.DialID(pb.GetID())
+	d.UserID = wtf.UserID(pb.GetUserID())
+	d.Name = pb.GetName()
+	d.Level = pb.GetLevel()
+	d.ModTime = time.Unix(0, pb.GetModTime()).UTC()
+
+	return nil
+}
