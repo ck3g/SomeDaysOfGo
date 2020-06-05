54caff2c63caa5a959fa4afa80ed893698c30d0d
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-05 10:07:08 +0200

    Add tests for bolt internal

---
+++ build-your-own-x/wtf-dial/bolt/internal/internal_test.go
@@ -0,0 +1,31 @@
+package internal_test
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/ck3g/SomeDaysOfGo/build-your-own-x/wtf-dial/bolt/internal"
+
+	"github.com/ck3g/SomeDaysOfGo/build-your-own-x/wtf-dial"
+)
+
+// Ensure dial cna be marshaled and unmarshaled.
+func TestMarchalDial(t *testing.T) {
+	v := wtf.Dial{
+		ID:      1,
+		UserID:  2,
+		Name:    "MYDIAL",
+		Level:   10.2,
+		ModTime: time.Now().UTC(),
+	}
+
+	var other wtf.Dial
+	if buf, err := internal.MarshalDial(&v); err != nil {
+		t.Fatal(err)
+	} else if err := internal.UnmarshalDial(buf, &other); err != nil {
+		t.Fatal(err)
+	} else if !reflect.DeepEqual(v, other) {
+		t.Fatalf("unexpected copy: %#v", other)
+	}
+}
