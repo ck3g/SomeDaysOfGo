8727e9bb388d926fdeee30a214fe796f5ae1fe9a
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-14 13:26:41 +0200

    Pick the sorting algorithm depending on items amount

--- testing-course/src/api/services/sort_service.go
+++ testing-course/src/api/services/sort_service.go
@@ -4,5 +4,9 @@ import "github.com/ck3g/SomeDaysOfGo/testing-course/src/api/utils/sort"
 
 // Sort provides a sort function
 func Sort(elements []int) {
-	sort.BubbleSort(elements)
+	if len(elements) <= 10000 {
+		sort.BubbleSort(elements)
+		return
+	}
+	sort.Sort(elements)
 }
