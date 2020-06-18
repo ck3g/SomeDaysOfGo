83eacd3a57ba4d1b63226265e91559992371fb12
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-18 10:39:03 +0200

    Add a function test to check the successful response

--- testing-course/src/api/test/locations_functional_test.go
+++ testing-course/src/api/test/locations_functional_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/ck3g/SomeDaysOfGo/testing-course/src/api/domain/locations"
 	"github.com/ck3g/SomeDaysOfGo/testing-course/src/api/utils/errors"
 	"github.com/mercadolibre/golang-restclient/rest"
 	"github.com/stretchr/testify/assert"
@@ -34,4 +35,60 @@ func TestGetCountries_NotFound(t *testing.T) {
 	assert.EqualValues(t, http.StatusNotFound, res.StatusCode)
 	assert.EqualValues(t, "not_found", apiErr.Error)
 	assert.EqualValues(t, "Country not found", apiErr.Message)
+}
+
+func TestGetCountries_OK(t *testing.T) {
+	successfulResponse := `
+	{
+		"id": "DE",
+		"name": "Germany",
+		"locale": "en_US",
+		"currency_id": "USD",
+		"decimal_separator": ".",
+		"thousands_separator": ",",
+		"time_zone": "GMT+02:00",
+		"geo_information": {},
+		"states": [
+			{ "id": "DE-NW", "name": "North Rhine West" },
+			{ "id": "DE-BW", "name": "Baden-Wurtemberg" },
+			{ "id": "DE-BY", "name": "Baviera" },
+			{ "id": "DE-BB", "name": "Brandeburgo" },
+			{ "id": "DE-HB", "name": "Bremen" },
+			{ "id": "DE-HH", "name": "Hamburgo" },
+			{ "id": "DE-HE", "name": "Hesse" },
+			{ "id": "DE-NI", "name": "Lower Saxony" },
+			{ "id": "DE-MV", "name": "Mecklenburg-Vorpo" },
+			{ "id": "DE-RP", "name": "Rhineland-Palatinate" },
+			{ "id": "DE-SL", "name": "Saarland" },
+			{ "id": "DE-SN", "name": "Saxony" },
+			{ "id": "DE-ST", "name": "Sajonia-Anhalt" },
+			{ "id": "DE-SH", "name": "Schleswig-Holstein" },
+			{ "id": "DE-TH", "name": "Thuringia" }
+		]
+		}
+	`
+
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "https://api.mercadolibre.com/countries/DE",
+		HTTPMethod:   http.MethodGet,
+		RespHTTPCode: http.StatusOK,
+		RespBody:     successfulResponse,
+	})
+
+	res, err := http.Get("http://localhost:8080/locations/countries/DE")
+
+	bytes, _ := ioutil.ReadAll(res.Body)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+
+	var country locations.Country
+	err = json.Unmarshal(bytes, &country)
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, http.StatusOK, res.StatusCode)
+	assert.EqualValues(t, "Germany", country.Name)
+	assert.EqualValues(t, "GMT+02:00", country.TimeZone)
+	assert.EqualValues(t, 15, len(country.States))
 }
