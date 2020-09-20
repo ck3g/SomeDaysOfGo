65c3bd7ff306cfb343cb2da98ab57bae6430a714
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-09-20 15:22:18 +0200

    Create a signed token

--- web-auth-and-encryption/07-jwt/main.go
+++ web-auth-and-encryption/07-jwt/main.go
@@ -24,6 +24,21 @@ func (u *UserClaims) Valid() error {
 	return nil
 }
 
-func main() {
+var key []byte
 
+func main() {
+	for i := 1; i <= 64; i++ {
+		key = append(key, byte(i))
+	}
+
+}
+
+func createToken(c *UserClaims) (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
+	signedToken, err := t.SignedString(key)
+	if err != nil {
+		return "", fmt.Errorf("Error in createToken when signing token")
+	}
+
+	return signedToken, nil
 }
