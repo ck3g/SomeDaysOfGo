04f22461509be56f1464a3fd08ce714658b2afb7
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-11-01 13:17:24 +0100

    Add a test example

---
+++ exploring-the-go-course/04-code-organization-for-decoupling/cmd/person/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/ck3g/SomeDaysOfGo/exploring-the-go-course/04-code-organization-for-decoupling/models"
+	"github.com/ck3g/SomeDaysOfGo/exploring-the-go-course/04-code-organization-for-decoupling/storage/mongo"
+)
+
+func TestPut(t *testing.T) {
+	mdb := mongo.DB{}
+	p := models.Person{
+		First: "John",
+	}
+
+	Put(mdb, 1, p)
+
+	got := mdb.Retrieve(1)
+	if got != p {
+		t.Fatalf("want %v, got %v", p, got)
+	}
+}
