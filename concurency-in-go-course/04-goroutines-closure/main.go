9174a88fc064dbde3ffbf214f9685ef0ae9f8fca
Vitali Tatarintev <Kalastiuz@gmail.com>
2021-04-03 13:13:02 +0200

    Add a goroutine closure example

---
+++ concurency-in-go-course/04-goroutines-closure/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	incr := func(wg *sync.WaitGroup) {
+		var i int
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			i++
+
+			fmt.Println("value of i:", i)
+		}()
+
+		fmt.Println("return from function")
+		return
+	}
+
+	incr(&wg)
+	wg.Wait()
+	fmt.Println("done")
+}
