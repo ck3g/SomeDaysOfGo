9268338eb3205446b656c7b2d4460351a89da6af
Vitali Tatarintev <Kalastiuz@gmail.com>
2021-04-03 13:00:34 +0200

    Add a goroutines client server example

---
+++ concurency-in-go-course/02-goroutines-client-server/server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			continue
+		}
+
+		// Withough goroutine the server will be able to serve only one client at a time
+		// handleConn(conn)
+		go handleConn(conn)
+	}
+}
+
+// handleConn - utility function
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, "response from server\n")
+		if err != nil {
+			return
+		}
+
+		time.Sleep(time.Second)
+	}
+}
