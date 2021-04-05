04868cf010b8e6a75188b858f89abaf2e5541524
Vitali Tatarintev <Kalastiuz@gmail.com>
2021-04-05 15:10:26 +0200

    Add sync atomic example

---
+++ concurency-in-go-course/16-sync-package-atomic/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	runtime.GOMAXPROCS(4)
+
+	var counter uint64
+	var wg sync.WaitGroup
+
+	// TODO: implement concurrency safe counter
+
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for c := 0; c < 1000; c++ {
+				// counter++ // without using atomic the counter value is going to be unpredictable
+				atomic.AddUint64(&counter, 1)
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("counter: %v\n", counter)
+}
