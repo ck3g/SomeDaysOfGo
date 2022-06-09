d00aa07108f50f4830f6ffec1ad55a98a99b5753
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-06-09 12:19:54 +0200

    Accept and print the commandline arguments

--- golang-for-devops-course/main.go
+++ golang-for-devops-course/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	args := os.Args[1:]
+
+	fmt.Printf("Hello world!\nArguments: %v\n", args)
 }
