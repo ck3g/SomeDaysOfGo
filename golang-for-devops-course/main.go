d5ed9d8210c6cd7db03a7aeb324fcadb961f7527
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-06-10 10:06:35 +0200

    Query an endpoint passed as argument and print its body

--- golang-for-devops-course/main.go
+++ golang-for-devops-course/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -9,9 +13,26 @@ func main() {
 	args := os.Args
 
 	if len(args) < 2 {
-		fmt.Println("Usage: ./app-name <argument>")
+		fmt.Println("Usage: ./app-name <url>")
 		os.Exit(1)
 	}
 
-	fmt.Printf("Hello world!\nArguments: %v\n", args[1:])
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
 }
