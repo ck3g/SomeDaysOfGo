d6cc8155cbdbc69d99937a2a84d76888c896a03b
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-09-23 11:19:50 +0200

    Add a WriteJSON function

--- building-a-module/toolkit/tools.go
+++ building-a-module/toolkit/tools.go
@@ -202,6 +202,7 @@ type JSONResponse struct {
 	Data    any    `json:"data,omitempty"`
 }
 
+// ReadJSON tries to read the body of a request and converts it from JSON into a go data variable
 func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
 	maxBytes := 1024 * 1024
 
@@ -257,6 +258,29 @@ func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error
 	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
 		return errors.New("body must contain only one JSON value")
+	}
+
+	return nil
+}
+
+// WriteJSON takes a response status code and arbitrary data writes json to the client
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
 	}
 
 	return nil
