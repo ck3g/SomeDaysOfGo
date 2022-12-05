03d8d3873ea161c82fbc1c1dbf17272e8ae06677
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-12-05 09:35:24 +0100

    Build nutritional score project

---
+++ nutritional-score-project/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	ns := GetNutritionalScore(NutritionalData{
+		Energy:              EnergyFromKcal(0),
+		Sugars:              SugarGram(10),
+		SaturatedFattyAcids: SaturatedFattyAcids(2),
+		Sodium:              SodiumMilligram(500),
+		Fruits:              FruitsPercent(60),
+		Fibre:               FibreGram(4),
+		Protein:             ProteinGram(2),
+	}, Food)
+
+	fmt.Printf("Nutritional score: %d\n", ns.Value)
+	fmt.Printf("Nutritional score: %s\n", ns.GetNutriScore())
+}
+
+func GetNutritionalScore(n NutritionalData, st ScoreType) NutritionalScore {
+	value := 0
+	positive := 0
+	negative := 0
+
+	if st != Water {
+		fruitPoints := n.Fruits.GetPoints(st)
+		fibrePoints := n.Fibre.GetPoints(st)
+
+		negative = n.Energy.GetPoints(st) + n.Sugars.GetPoints(st) + n.SaturatedFattyAcids.GetPoints(st) + n.Sodium.GetPoints(st)
+		positive = fruitPoints + fibrePoints + n.Protein.GetPoints(st)
+
+		if st == Cheese {
+			value = negative - positive
+		} else {
+			if negative > 11 && fruitPoints < 5 {
+				value = negative - positive - fruitPoints
+			} else {
+				value = negative - positive
+			}
+		}
+	}
+
+	return NutritionalScore{
+		Value:     value,
+		Positive:  positive,
+		Negative:  negative,
+		ScoreType: st,
+	}
+}
