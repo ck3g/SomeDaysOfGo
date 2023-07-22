b1c5e438375da737a0bb66f2180d49e4656592c2
Vitali Tatarintev <Kalastiuz@gmail.com>
2023-07-22 12:32:13 +0300

    Add store specific discounts

--- coffeeco/internal/store/store.go
+++ coffeeco/internal/store/store.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"context"
+
 	"github.com/google/uuid"
 
 	coffeeco "coffeeco/internal"
@@ -10,4 +12,17 @@ type Store struct {
 	ID              uuid.UUID
 	Location        string
 	ProductsForSale []coffeeco.Product
+}
+
+type Service struct {
+	repo Repository
+}
+
+func (s Service) GetStoreSpecificDiscount(ctx context.Context, storeID uuid.UUID) (float32, error) {
+	dis, err := s.repo.GetStoreDiscount(ctx, storeID)
+	if err != nil {
+		return 0, err
+	}
+
+	return float32(dis), nil
 }
